7c44ac75e1c0c9a109bdc9dae7dfa58c967eb25e
saminakhan102 <57982631+saminakhan102@users.noreply.github.com>
2023-03-06 02:58:17 +0500

    Update AppDelegate.swift

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,34 +1,13 @@
-import SwiftUI
+import UIKit
 import Flutter
 
-struct ContentView: View {
-  // Access the AppDelegate using an EnvironmentObject.
-  @EnvironmentObject var appDelegate: AppDelegate
-
-  var body: some View {
-    Button("Show Flutter!") {
-      openFlutterApp()
-    }
-  }
-
-func openFlutterApp() {
-    // Get the RootViewController.
-    guard
-      let windowScene = UIApplication.shared.connectedScenes
-        .first(where: { $0.activationState == .foregroundActive && $0 is UIWindowScene }) as? UIWindowScene,
-      let window = windowScene.windows.first(where: \.isKeyWindow),
-      let rootViewController = window.rootViewController
-    else { return }
-
-    // Create the FlutterViewController.
-    let flutterViewController = FlutterViewController(
-      // Access the Flutter Engine via AppDelegate.
-      engine: appDelegate.flutterEngine,
-      nibName: nil,
-      bundle: nil)
-    flutterViewController.modalPresentationStyle = .overCurrentContext
-    flutterViewController.isViewOpaque = false
-
-    rootViewController.present(flutterViewController, animated: true)
+@UIApplicationMain
+@objc class AppDelegate: golfcandieDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
